62b4f3af1675e534b0e7014d47ebcd2266cd5873
Rafel Villarreal <rvillarreal416@gmail.com>
2019-01-15 18:34:40 -0500

    Header component created

--- App.js
+++ App.js
@@ -1,21 +1,23 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { 
+  StyleSheet, 
+  Text, View 
+} from 'react-native'
+import Header from '@/components/Header'
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Header title="Time Tracker"/>
       </View>
-    );
+    )
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#fff'
   },
-});
+})
---
+++ src/components/Header.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import { Constants } from 'expo'
+import {
+  View,
+  Text,
+  Platform,
+  StyleSheet
+} from 'react-native'
+
+function Header ({ title }) {
+  return (
+    <View >
+      <View style={styles.statusBar}></View>
+      <View style={styles.container}>
+        <Text style={styles.title}>{ title }</Text>
+      </View>
+    </View>
+  )
+} 
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    height: Platform.OS === 'ios' ? 50 : 56,
+    backgroundColor: '#9b59b6',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: '600',
+    color: 'white'
+  },
+  statusBar: {
+    height: Constants.statusBarHeight,
+    backgroundColor: '#9b59b6',
+  }
+})
+
+export default Header
